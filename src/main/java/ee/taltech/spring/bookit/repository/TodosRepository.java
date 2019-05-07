db5b755e1e7c3c68d36dccbbb309e658a1ab769e
nilst <nitirs@ttu.ee>
2019-05-07 08:38:58 +0300

    Book application

--- src/main/java/ee/taltech/spring/bookit/repository/TodosRepository.java
+++
@@ -1,7 +0,0 @@
-package ee.taltech.spring.bookit.repository;
-
-import ee.taltech.spring.bookit.domain.Todo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodosRepository extends JpaRepository<Todo, Long> {
-}
--- src/main/java/ee/taltech/spring/bookit/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package ee.taltech.spring.bookit.repository;
-
-import ee.taltech.spring.bookit.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
